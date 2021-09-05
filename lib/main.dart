d073225f919c63c67d7f6413321b0c7096c64f20
tomoki1590 <kaguratomato1@gmail.com>
2021-09-06 07:27:52 +0900

    restart

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
+void main() {runApp(MyApp());}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  const MaterialApp(
+      title: "todoApp",
+      home: HomeScreen(),
+    );
+  }
+}
